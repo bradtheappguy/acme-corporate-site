4e97a2e0d1655098ae3ea1f47d2eb8ac9acbca75
Laura Jensen <info@designlaurels.com>
2015-03-02 02:49:31 -0800

    updates for ACME and cleanup of MyRadar website

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php 
-$emailTo = 'mail@example.com';
-$siteTitle = 'Your Site Title';
+$emailTo = 'contact@acmeaom.com';
+$siteTitle = 'ACME Atron-O-Matic, LLC Website';
 
 error_reporting(E_ALL ^ E_NOTICE); // hide all basic notices from PHP
 
